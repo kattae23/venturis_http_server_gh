15826e0b1874ea09653b01da9e554535dae95996
Alfonzo Hernandez <111934617+kattae23@users.noreply.github.com>
2024-05-03 23:51:43 -0400

    feat: fixing errors

--- src/data/mongo-db/index.ts
+++ src/data/mongo-db/index.ts
@@ -3,7 +3,11 @@ import envs from '../../config/envs';
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(envs.MONGO_URL);
+    await mongoose.connect(envs.MONGO_URL, {
+      maxIdleTimeMS: 100000,
+      ssl: true,
+      autoCreate: true,
+    });
     console.log('- Connected to the database adventure (MongoDB)');
   } catch (error) {
     console.log(error);
