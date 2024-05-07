45821aa1bca024e11e561f79493d0ae38ee9a4c2
Alfonzo Hernandez <111934617+kattae23@users.noreply.github.com>
2024-05-07 01:30:28 -0400

    feat: change ssl to false on prod

--- src/data/mongo-db/index.ts
+++ src/data/mongo-db/index.ts
@@ -4,8 +4,7 @@ import envs from '../../config/envs';
 const dbConnection = async () => {
   try {
     await mongoose.connect(envs.MONGO_URL, {
-      // ssl: envs.PROD ?? envs.PROD,
-      ssl: false,
+      ssl: envs.PROD && !envs.PROD,
     });
     console.log('- Connected to the database adventure (MongoDB)');
   } catch (error) {
