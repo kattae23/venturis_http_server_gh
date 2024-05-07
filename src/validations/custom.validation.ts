70c2a54a4614b720365cabc0f300fb621be1a7b7
Alfonzo Hernandez <111934617+kattae23@users.noreply.github.com>
2024-05-07 01:12:28 -0400

    feat: last changes

--- src/validations/custom.validation.ts
+++ src/validations/custom.validation.ts
@@ -2,31 +2,31 @@ import Joi from 'joi';
 
 const objectId = (value: string, helpers: Joi.CustomHelpers<any>) => {
   if (!value.match(/^[0-9a-fA-F]{24}$/)) {
-    return helpers.message('"{{#label}}" must be a valid mongo id');
+    return helpers.error('"{{#label}}" must be a valid mongo id');
   }
   return value;
 };
 
 const password = (value: string, helpers: Joi.CustomHelpers<any>) => {
   if (value.length < 8) {
-    return helpers.message('La contraseña debe tener al menos 8 caracteres');
+    return helpers.error('La contraseña debe tener al menos 8 caracteres');
   }
 
   if (!/[A-Z]/.test(value)) {
-    return helpers.message(
+    return helpers.error(
       'La contraseña debe contener al menos una letra mayúscula',
     );
   }
   if (!/[a-z]/.test(value)) {
-    return helpers.message(
+    return helpers.error(
       'La contraseña debe contener al menos una letra minúscula',
     );
   }
   if (!/[0-9]/.test(value)) {
-    return helpers.message('La contraseña debe contener al menos un número');
+    return helpers.error('La contraseña debe contener al menos un número');
   }
   if (!/[!@#$%^&*]/.test(value)) {
-    return helpers.message(
+    return helpers.error(
       'La contraseña debe contener al menos un carácter especial',
     );
   }
